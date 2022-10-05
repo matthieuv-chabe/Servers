7af05d4b8ddbabde980b379be1aee2223c1971e4
Matthieu V <matthieu.vancayzeele@gmail.com>
2022-10-05 23:17:33 +0200

    refactor

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,13 +2,12 @@
 //
 
 #include <WinSock2.h>
-#include <Ws2tcpip.h>
+#include <WS2tcpip.h>
 #include <Windows.h>
 
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <algorithm>
 #include <functional>
 
 #include <string>
@@ -17,248 +16,250 @@
 
 #pragma comment (lib, "Ws2_32.lib")
 
-#define DEFAULT_BUFLEN 4096
-#define DEFAULT_PORT "27015"
+constexpr auto DEFAULT_BUFLEN = 10;
 
-class TcpServer
+class tcp_server
 {
-
 private:
 
-	WSADATA wsaData;
-	int iResult;
+	WSADATA wsa_data_;
 
-	SOCKET ListenSocket = INVALID_SOCKET;
+	SOCKET listen_socket_ = INVALID_SOCKET;
 
-	addrinfo* result = NULL;
-	addrinfo hints;
-
-	int iSendResult;
-	char recvbuf[DEFAULT_BUFLEN];
-	int recvbuflen = DEFAULT_BUFLEN;
-
-	std::vector<SOCKET> sockets;				// List of all sockets
-	std::thread AcceptThread;					// The thread that accepts new connections
-	std::vector<std::thread> ListenerThread;	// One thread per connection
+	std::vector<SOCKET> sockets_;				// List of all sockets
+	std::thread accept_thread_;					// The thread that accepts new connections
+	std::vector<std::thread> listener_thread_;	// One thread per connection
 
 public:
 
 	// Callbacks
-	std::vector<std::function<void(SOCKET)>> cbOnConnect;
-	std::vector<std::function<void(SOCKET)>> cbOnDisconnect;
-	std::vector<std::function<void(SOCKET, const char* /* message */, size_t /* message len */)>> cbOnRecv;
+	std::vector<std::function<void(SOCKET)>> cb_on_connect;
+	std::vector<std::function<void(SOCKET)>> cb_on_disconnect;
+	std::vector<std::function<void(SOCKET, const char* /* message */, size_t /* message len */)>> cb_on_recv;
 
 public:
 
-	TcpServer()
+	tcp_server()
 	{
 	}
 
-	void StartServer(const std::string_view&& port)
+	void start_server(const char* port)
 	{
-		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-		if (iResult)
+		auto i_result = WSAStartup(MAKEWORD(2, 2), &wsa_data_);
+		if (i_result)
 		{
 			throw std::exception();
 		}
 
+		addrinfo hints;
 		ZeroMemory(&hints, sizeof(hints));
 		hints.ai_family = AF_INET;
 		hints.ai_socktype = SOCK_STREAM;
 		hints.ai_protocol = IPPROTO_TCP;
 		hints.ai_flags = AI_PASSIVE;
 
-		iResult = getaddrinfo(NULL, port.data(), &hints, &result);
-		if (iResult)
+		addrinfo* result;
+		i_result = getaddrinfo(nullptr, port, &hints, &result); // pNodeName can be IP to bind to
+		if (i_result)
 		{
 			WSACleanup();
 			throw std::exception();
 		}
 
 		// Create a SOCKET for the server to listen for client connections.
-		ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-		if (ListenSocket == INVALID_SOCKET)
+		listen_socket_ = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+		if (listen_socket_ == INVALID_SOCKET)
 		{
-			printf("socket failed with error: %ld\n", WSAGetLastError());
+			printf("socket failed with error: %d\n", WSAGetLastError());
 			freeaddrinfo(result);
 			WSACleanup();
 			throw std::exception();
 		}
 
 		// Setup the TCP listening socket
-		iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-		if (iResult == SOCKET_ERROR)
+		i_result = bind(listen_socket_, result->ai_addr, static_cast<int>(result->ai_addrlen));
+		if (i_result == SOCKET_ERROR)
 		{
 			printf("bind failed with error: %d\n", WSAGetLastError());
 			freeaddrinfo(result);
-			closesocket(ListenSocket);
+			closesocket(listen_socket_);
 			WSACleanup();
 			throw std::exception();
 		}
 
 		freeaddrinfo(result);
 
-		iResult = listen(ListenSocket, SOMAXCONN);
-		if (iResult == SOCKET_ERROR)
+		i_result = listen(listen_socket_, SOMAXCONN);
+		if (i_result == SOCKET_ERROR)
 		{
 			printf("listen failed with error: %d\n", WSAGetLastError());
-			closesocket(ListenSocket);
+			closesocket(listen_socket_);
 			WSACleanup();
 			throw std::exception();
 		}
 
-		AcceptThread = std::thread(std::bind(&TcpServer::AcceptThreadFx, this));
-		AcceptThread.detach();
+		accept_thread_ = std::thread([this] { accept_thread_fx(); });
+		accept_thread_.detach();
 	}
 
 private:
 
-	void AcceptThreadFx()
+	[[noreturn]]
+	void accept_thread_fx()
 	{
 		while (true)
 		{
-			auto client_socket = accept(this->ListenSocket, (sockaddr*)NULL, (int*)NULL);
+			auto client_socket = accept(this->listen_socket_, nullptr, nullptr);
 			if (client_socket == INVALID_SOCKET)
 			{
-				throw std::exception();
 				WSACleanup();
+				throw std::exception();
 			}
 
-			this->sockets.push_back(client_socket);
+			this->sockets_.push_back(client_socket);
 
-			for (const auto f : cbOnConnect)
+			for (const auto& f : cb_on_connect)
 				f(client_socket);
 
-			std::thread listener_thread(std::bind(&TcpServer::ListenerThreadFx, this, client_socket));
+			std::thread listener_thread([this, client_socket] { listener_thread_fx(client_socket); });
 			listener_thread.detach();
 		}
 	}
 
-	void ListenerThreadFx(SOCKET client_socket)
+	void listener_thread_fx(const SOCKET client_socket)
 	{
+
 		while (true)
 		{
-			iResult = recv(client_socket, recvbuf, recvbuflen, 0);
+			char recvbuf[DEFAULT_BUFLEN];
+			constexpr size_t recvbuflen = DEFAULT_BUFLEN;
 
-			if (iResult > 0)
+			if (const auto i_result = recv(client_socket, recvbuf, recvbuflen, 0); i_result > 0)
 			{
-				// std::cout << "Recv " << iResult << " bytes" << std::endl;
-				for (const auto &f : cbOnRecv)
-					f(client_socket, recvbuf, iResult);
+				for (const auto& f : cb_on_recv)
+					f(client_socket, recvbuf, i_result);
 			}
-			else if (iResult == 0)
+			else if (i_result == 0)
 			{
-				for (const auto f : cbOnDisconnect)
+				for (const auto& f : cb_on_disconnect)
 					f(client_socket);
-				
+
 				std::cout << "connected" << std::endl;
 
-				this->sockets.erase(std::remove(sockets.begin(), sockets.end(), client_socket), sockets.end());
+				this->sockets_.erase(std::ranges::remove(sockets_, client_socket).begin(), sockets_.end());
 				break;
 			}
 			else
 			{
-				for (const auto f : cbOnDisconnect)
+				for (const auto f : cb_on_disconnect)
 					f(client_socket);
 
 				std::cout << "disconnected" << std::endl;
 
-				this->sockets.erase(std::remove(sockets.begin(), sockets.end(), client_socket), sockets.end());
+				this->sockets_.erase(std::ranges::remove(sockets_, client_socket).begin(), sockets_.end());
 				break;
 
-				std::cout << iResult << " " << WSAGetLastError() << std::endl;
+				std::cout << i_result << " " << WSAGetLastError() << std::endl;
 			}
-
 		}
-
 	}
 
 public:
 
-	void Broadcast(const std::string message)
+	void broadcast(const std::string& message) const
 	{
-		Broadcast(message.c_str(), message.size()+1);
+		broadcast(message.c_str(), message.size() + 1);
 	}
 
-	void Broadcast(const char* message, const size_t size)
+	void broadcast(const char* message, const size_t size) const
 	{
-		for (const auto s : sockets)
+		for (const auto s : sockets_)
 		{
-			Send(s, message, size);
+			send(s, message, size);
 		}
 	}
 
-	static void Send(SOCKET s, std::string message)
+	static void send(const SOCKET s, const std::string& message)
 	{
 		std::cout << "sending " << message << std::endl;
-		Send(s, message.c_str(), message.size() + 1);
+		send(s, message.c_str(), message.size() + 1);
 	}
 
-	static void Send(SOCKET s, const char* message, const size_t size)
+	static void send(const SOCKET s, const char* message, const size_t size)
 	{
-		const auto iSendResult = send(s, message, size, MSG_OOB);
-		if (iSendResult != size)
+		if (const auto i_send_result = ::send(s, message, size, MSG_OOB); i_send_result != size) [[unlikely]]
 		{
 			std::cout << "Sending fail :" << WSAGetLastError() << std::endl;
 			closesocket(s);
 		}
 	}
 
-	static void Close(SOCKET s)
+	static void close(SOCKET s)
 	{
 		std::cout << "closing" << std::endl;
 		shutdown(s, SD_BOTH);
 	}
-
 };
 
-class HttpServer
+class http_server
 {
 public:
 
-	struct HttpRequest {
-		enum RequestType { GET, POST, UPDATE, UNK };
+	struct http_request {
+
+		enum request_type { get, post, update, unk };
 
 		SOCKET s;
-		TcpServer *tcp;
+		tcp_server* tcp;
 
 		// Header frequently used stuff
 		size_t contentlength{ 0 };
-		
-		RequestType type;
+
+		request_type type;
 		std::string destination;
-		std::map<std::string, std::string> headers {{"request",""}};
+		std::map<std::string, std::string> headers{ {"request",""} };
 		std::string content;
 	};
 
-	enum HttpParsingState {
-		IN_HEADER,
-		END_OF_HEADERS,
-		IN_BODY,
+	enum http_parsing_state {
+		in_header,
+		end_of_headers,
+		in_body,
 	};
 
-	TcpServer tcp;
+	tcp_server tcp;
 
-	std::vector<std::function<void(HttpRequest& req)>> cbOnHttpRequestDone;
+	std::vector<std::function<void(http_request& req)>> cbOnHttpRequestDone;
 
-	void StartServer(const char* port)
+	void start_server(const char* port)
 	{
 		using namespace std::placeholders;
-		tcp.cbOnRecv.push_back(std::bind(&HttpServer::OnTcpMsgReceived, this, _1, _2, _3));
+		tcp.cb_on_recv.emplace_back([this](auto&& ph1, auto&& ph2, auto&& ph3)
+			{
+				on_tcp_msg_received(
+					std::forward<decltype(ph1)>(ph1),
+					std::forward<decltype(ph2)>(ph2),
+					std::forward<decltype(ph3)>(ph3));
+			});
 
-		tcp.StartServer(port);
+		tcp.start_server(port);
 	}
 
-	std::map<SOCKET, HttpParsingState> message_parsing_state;
-	std::map<SOCKET, std::string> message_parts;
-	std::map<SOCKET, HttpRequest> message_request;
+private: // TODO : this one is the correct one lol
+public:
 
-	void HandleHttpHeaderLine(SOCKET& socket, const std::string &&line)
+	std::map<SOCKET, http_parsing_state> message_parsing_state;
+	std::map<SOCKET, std::string> message_parts;
+	std::map<SOCKET, http_request> message_request;
+
+private:
+
+	void handle_http_header_line(const SOCKET& socket, const std::string&& line)
 	{
 		std::cout << __FUNCTION__ << line << std::endl;
 
 		if (line.size() < 3) return;
-		
+
 		auto& mr = message_request[socket];
 
 		// message_request[socket].headers.append(line).append("\r\n");
@@ -267,24 +268,23 @@ public:
 		// For the "GET /" line that doesnt contain ":" char
 		if (idx == std::string::npos) [[unlikely]]
 		{
-
-			int offset = 0;
+			size_t offset = 0;
 			switch (line[0])
 			{
 			case 'G':
-				mr.type = HttpRequest::RequestType::GET;
+				mr.type = http_request::request_type::get;
 				offset = 4; // +1 for the space between the keyword and the path
 				break;
 			case 'P':
-				mr.type = HttpRequest::RequestType::POST;
+				mr.type = http_request::request_type::post;
 				offset = 5;
 				break;
 			default:
-				mr.type = HttpRequest::RequestType::UNK;
+				mr.type = http_request::request_type::unk;
 				offset = line.find_first_of(' ');
 			}
 
-			auto otherspace = line.find_first_of(' ', offset + 1);
+			const auto otherspace = line.find_first_of(' ', offset + 1);
 			mr.destination = line.substr(offset, otherspace - offset);
 
 			mr.headers["request"] = line;
@@ -304,11 +304,10 @@ public:
 		}
 	}
 
-	bool OnHttpHeaderMessageReceived(SOCKET socket, std::string &data)
+	bool on_http_header_message_received(const SOCKET socket, std::string& data)
 	{
-
 		std::cout << __FUNCTION__ << data << std::endl;
-		
+
 		if (data.size() < 2) return false;
 
 		auto datasize = data.size();
@@ -316,73 +315,68 @@ public:
 		{
 			if (data[i] != '\n') [[likely]] continue;
 
-			HandleHttpHeaderLine(socket, data.substr(0, i - 1)); // Skip the \r before \n. There's ALWAYS a \r before.
-			data = data.substr(i + 1);
-
-			datasize -= (i + 1);
-
-			if (datasize > 1 && data[1] == '\n') [[unlikely]]  // TODO : if the first \r\n and this one are in separate packets, it wont trigger.
+			if (datasize > 2) [[likely]]   // Most of the time, some data will be received for the header
 			{
-				// std::cout << "Done" << std::endl;
-				data = data.substr(2); // skip \r\n
-				return true;
+				handle_http_header_line(socket, data.substr(0, i - 1)); // Skip the \r before \n. There's ALWAYS a \r before.
+				data = data.substr(i + 1);
+				datasize -= (i + 1);
 			}
+
+				if (datasize > 1 && data[1] == '\n') [[unlikely]]
+				{
+					data = data.substr(2); // skip \r\n
+					return true;
+				}
 
 			i = 0; // Restart the loop
 		}
 		return false;
 	}
 
-	void OnTcpMsgReceived(SOCKET socket, const char *data, size_t len)
+	void on_tcp_msg_received(const SOCKET socket, const char* data, size_t len)
 	{
-		// std::cout << "recv tcp msg" << std::endl;
-		auto msg = &message_parts[socket];
+		const auto msg = &message_parts[socket];
 		msg->append(data, len);
 
-		// std::cout << "Cur message: " << std::endl << *msg << std::endl;
-
-		begin_of_switch:
+	begin_of_switch:
 		switch (message_parsing_state[socket])
 		{
-			case IN_HEADER:
+		case in_header:
+		{
+			if (on_http_header_message_received(socket, *msg))	// If done parsing
 			{
-				if (OnHttpHeaderMessageReceived(socket, *msg))	// If done parsing
-				{
-					message_parsing_state[socket] = END_OF_HEADERS;
-					goto begin_of_switch;
-				}
-				break;  
+				message_parsing_state[socket] = end_of_headers;
+				goto begin_of_switch;
 			}
-
-			case END_OF_HEADERS:
-				// std::cout << "sending answer" << std::endl;
-				// tcp.Send(socket, "HTTP/1.1 204 No Content \r\nServer-Timing: matthieu;dur=10, thomas;dur=200.2\r\n\r\n");
-
-				if (message_request[socket].contentlength != 0 && message_request[socket].contentlength > msg->size() + 1)
-				{
-					std::cout << "Body not received in total. Waiting. " << msg->size() << "/" << message_request[socket].contentlength << std::endl;
-				}
-
-				message_request[socket].content = *msg;
-				
-				for (const auto& f : cbOnHttpRequestDone)
-					f(message_request[socket]);
-
-				break;
-
-			case IN_BODY:
-				std::cout << ".";
-				break;
+			break;
 		}
 
-	}
+		case end_of_headers:
 
+			if (message_request[socket].contentlength != 0 && message_request[socket].contentlength > msg->size() + 1)
+			{
+				std::cout << "Body not received in total. Waiting. " << msg->size() << "/" << message_request[socket].contentlength << std::endl;
+				return;
+			}
+
+			message_request[socket].content = *msg;
+
+			for (const auto& f : cbOnHttpRequestDone)
+				f(message_request[socket]);
+
+			break;
+
+		case in_body:
+			std::cout << ".";
+			break;
+		}
+	}
 };
 
-class WebSocketServer
+class websocket_server
 {
 public:
-	inline static const std::string WS_GUID { "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" };
+	inline static const std::string WS_GUID{ "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" };
 };
 
 class endpoint
@@ -390,18 +384,23 @@ class endpoint
 public:
 
 	SOCKET s;
-	sockaddr_in sockaddrin = { 0 };
+	sockaddr_in sockaddrin = { 0, 0, 0, 0 };
 
-	endpoint(SOCKET in) : s(in) {}
+	endpoint(const SOCKET in) : s(in) {}
 
 	SOCKET data() const
 	{
 		return s;
 	}
 
-	const std::string Ip()
+	operator SOCKET() const
 	{
-		if (sockaddrin.sin_port == 0) GrabSockaddrinData();
+		return s;
+	}
+
+	std::string ip()
+	{
+		if (sockaddrin.sin_port == 0) grab_sockaddrin_data();
 
 		const auto& x = sockaddrin.sin_addr.S_un.S_un_b;
 
@@ -411,75 +410,86 @@ public:
 		return ss.str();
 	}
 
-	const uint16_t Port()
+	uint16_t port()
 	{
-		if (sockaddrin.sin_port == 0) GrabSockaddrinData();
+		if (sockaddrin.sin_port == 0) grab_sockaddrin_data();
 
 		return sockaddrin.sin_port;
 	}
 
-	void GrabSockaddrinData()
+private:
+
+	void grab_sockaddrin_data()
 	{
 		int addrlen = sizeof(sockaddrin);
-		getsockname(s, (sockaddr*)&sockaddrin, &addrlen);
+		getsockname(s, reinterpret_cast<sockaddr*>(&sockaddrin), &addrlen);
 	}
 };
 
-class WebServer
+class web_server
 {
-	
-	using HttpRequestCallback_t = bool(HttpServer::HttpRequest&);
+	using http_request_callback_t = bool(http_server::http_request&);
+
+private:
+
+	http_server http_;
+	std::map<std::string_view, std::function<http_request_callback_t>> gets_, posts_, reqs_;
 
 public:
 
-	HttpServer http;
-	std::map<std::string_view, std::function<HttpRequestCallback_t>> Gets, Posts, Reqs;
-
-	struct WebRequest
+	struct web_request
 	{
-		HttpServer::HttpRequest http;
+		http_server::http_request http;
 	};
 
-	void StartServer(const char* port)
+	void start_server(const char* port)
 	{
 		using namespace std::placeholders;
-		http.cbOnHttpRequestDone.push_back(std::bind(&WebServer::HandleHttpRequest, this, _1));
-		http.tcp.cbOnConnect.push_back([this](SOCKET s) { http.message_request[s].tcp = &this->http.tcp; http.message_request[s].s = s; });
 
-		http.StartServer(port);
+		// Add callbacks
+
+		http_.cbOnHttpRequestDone.emplace_back([this](auto&& ph1)
+			{
+				handle_http_request(std::forward<decltype(ph1)>(ph1));
+			});
+
+		http_.tcp.cb_on_connect.emplace_back([this](const SOCKET s)
+			{
+				http_.message_request[s].tcp = &this->http_.tcp;
+				http_.message_request[s].s = s;
+			});
+
+		http_.start_server(port);
 	}
 
-	void HandleHttpRequest(HttpServer::HttpRequest &httpReq)
+	static void handle_http_request(const http_server::http_request& http_req)
 	{
-		endpoint ep(httpReq.s);
-		std::cout << "Received HTTP request from " << ep.Ip() << ":" << ep.Port() << std::endl;
+		endpoint ep(http_req.s);
+		std::cout << "Received HTTP request from " << ep.ip() << ":" << ep.port() << std::endl;
 
-		if (httpReq.type == HttpServer::HttpRequest::RequestType::GET) [[likely]]
+		if (http_req.type == http_server::http_request::request_type::get) [[likely]]
 		{
 			std::cout << "get" << std::endl;
-			httpReq.tcp->Send(httpReq.s, "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello worssssssssssssssssssssssssssssssld!"); // mdlol
+			tcp_server::send(http_req.s, "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello worssssssssssssssssssssssssssssssld!"); // mdlol
 		}
-		else if (httpReq.type == HttpServer::HttpRequest::RequestType::POST)
+		else if (http_req.type == http_server::http_request::request_type::post)
 		{
-			std::cout << "post: " << httpReq.content << std::endl;
-			httpReq.tcp->Send(httpReq.s, "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello worssssssssssssssssssssssssssssssld!");
+			std::cout << "post: " << http_req.content << std::endl;
+			tcp_server::send(http_req.s, "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello worssssssssssssssssssssssssssssssld!");
 			// httpReq.tcp->Close(httpReq.s);
 		}
 	}
 
-	void Get(const std::string_view path, std::function<HttpRequestCallback_t>&& callback)
+	void get(const std::string_view path, std::function<http_request_callback_t>&& callback)
 	{
-		Gets[path] = callback;
+		gets_[path] = callback;
 	}
-
 };
-
-
 
 int main()
 {
-	WebServer ts;
-	ts.StartServer("81");
+	web_server ts;
+	ts.start_server("81");
 
 	while (true)
 	{
